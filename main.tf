f538f051d99f0730127287ba973f0faa06ce9956
javaqueries <chetan.itmuniverse@gmail.com>
2021-03-18 09:00:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 
-
 provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+  access_key = "AKIAV6PBJZ4RQBXXL4PD"
+  secret_key = "D3aFwNsSdkgLKU0YeY6NhkZM64VAy0MfJly0p15q"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0915bcb5fa77e4892"
+  instance_type = "t2.micro"
 }
 
 
