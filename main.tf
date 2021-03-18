fbafcb22a3dc9983b583ee723710a1ebc39e60e4
javaqueries <chetan.itmuniverse@gmail.com>
2021-03-18 09:19:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 provider "aws" {
-  access_key = "AKIAV6PBJZ4RQBXXL4PD"
-  secret_key = "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"
+  access_key = "AKIAV6PBJZ4RU6NQUJNQ"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
