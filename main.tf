e76da094207590491c544a994690940200c25788
javaqueries <chetan.itmuniverse@gmail.com>
2021-03-18 08:56:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      access_key = "AKIAV6PBJZ4RQBXXL4PD"
+      secret_key = "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"
       version = "~> 3.27"
     }
   }
